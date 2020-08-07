3238d497104ae5a3df5eb22381e2020fc4899750
Claydameron <66542550+Claydameron@users.noreply.github.com>
2020-08-07 11:10:42 +0530

    Add files via upload

---
+++ BoxBlue.js
@@ -0,0 +1,29 @@
+class BoxB {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.visibility = 255;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    angleMode(RADIANS);
+    strokeWeight(1);
+    stroke(0);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("lightBlue");
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+}
---
+++ BoxYellow.js
@@ -0,0 +1,40 @@
+class BoxY {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.visibility = 255;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    if(this.body.speed<3) {
+    push();
+    translate(pos.x, pos.y);
+    angleMode(RADIANS);
+    strokeWeight(1);
+    stroke(0);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("yellow");
+    rect(0, 0, this.width, this.height);
+     } else {
+        World.remove(world,this.body);
+        push();
+        tint(255,this.visibility = this.visibility - 5);
+        image(this.image,this.body.position.x,this.body.position.y,50,50);
+        pop();
+      }
+      pop();
+    
+  }
+
+  
+}
